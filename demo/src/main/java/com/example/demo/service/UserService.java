dc2a0160e4d157737b71bfe227f06dc1862c82e6
Hank <shliu1066@gmail.com>
2020-07-22 15:52:47 +0800

    Conneting to SSMS & Search database by mybatis But only can compile on Spring Boot Application

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.UserMapper;
+import com.example.demo.model.User;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+
+	public List<User> getAllUser(){
+		
+		List<User> users = userMapper.getAllUser();
+		
+		return users;
+	}
+}
